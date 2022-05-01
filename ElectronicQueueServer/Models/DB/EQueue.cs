935fd702889c853191371f7f6c7ecfafa972dbff
Evgeny.Shinkarev@urfu.me <Evgeny.Shinkarev@urfu.me>
2022-05-01 13:34:38 +0500

    Пееред переходом на .net 6

---
+++ ElectronicQueueServer/Models/DB/EQueue.cs
@@ -0,0 +1,18 @@
+﻿using ElectronicQueueServer.Models.DB;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ElectronicQueueServer.Models
+{
+    public class EQueue
+    {
+        [JsonProperty("id")]
+        public ObjectId Id { get; set; }
+        [JsonProperty("creatorId")]
+        public ObjectId CreatorId { get; set; }
+        [JsonProperty("records")]
+        public IEnumerable<EQueueRecord> Records { get; set; }
+    }
+}
---
+++ ElectronicQueueServer/Models/DB/EQueueRecord.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System;
+
+namespace ElectronicQueueServer.Models.DB
+{
+    public class EQueueRecord
+    {
+        [JsonProperty("id")]
+        public ObjectId Id { get; set; }
+        [JsonProperty("registeredId")]
+        public ObjectId RegisteredId { get; set; }
+        [JsonProperty("receiptTimeBegin")]
+        public TimeOnly ReceiptTimeBegin { get; set; }
+        [JsonProperty("receiptTimeEnd")]
+        public DateTime ReceiptTimeEnd { get; set; }
+
+        [JsonProperty("recordType")]
+        public string RecordType { get; set; }
+
+        [JsonProperty("info")]
+        public object Info { get; set; }
+
+        public static class RecordTypeName
+        {
+            public const string Reception = "reception";
+            public const string Rest = "rest";
+        }
+    }
+}
